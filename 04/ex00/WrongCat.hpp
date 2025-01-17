e7f9b7e842de9cb9086f001a84e563d380541115
aavduli <aavduli@42lausanne.ch>
2025-01-17 12:15:23 +0100

    correction main

--- 04/ex00/WrongCat.hpp
+++ 04/ex00/WrongCat.hpp
@@ -1,32 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.cpp                                           :+:      :+:    :+:   */
+/*   WrongCat.hpp                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: aavduli <aavduli@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/01/16 16:58:48 by aavduli           #+#    #+#             */
-/*   Updated: 2025/01/17 12:13:31 by aavduli          ###   ########.fr       */
+/*   Created: 2025/01/17 12:14:46 by aavduli           #+#    #+#             */
+/*   Updated: 2025/01/17 12:14:49 by aavduli          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "animal.hpp"
-#include "dog.hpp"
-#include "cat.hpp"
-
-int	main() {
-
-	{
-		Animal* dog = new Dog();
-		Animal* cat = new Cat();
-
-		std::cout << dog->getType() << " " << std::endl;
-		std::cout << cat->getType() << " " << std::endl;
-		dog->makeSound();
-		cat->makeSound();
-
-		delete (dog);
-		delete (cat);
-	}
-	return 0;
-}
