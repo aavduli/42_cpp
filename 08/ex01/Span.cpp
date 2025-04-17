94de982e460d53270bfa9c8b7d1014bf024bc607
Avdyl Avduli <aavduli@c3r1s1.42lausanne.ch>
2025-04-17 17:01:38 +0200

    Span ex01 half

--- 08/ex01/Span.cpp
+++ 08/ex01/Span.cpp
@@ -4,11 +4,9 @@ Span::Span() {
 	std::cout << "Default constructor for Span has been called" << std::endl;
 }
 
-Span::Span() {
-}
+Span::Span(int n) : _maxSize(n) {}
 
-Span::Span(const Span &other) : { //copy constructor
-}
+Span::Span(const Span &other) : _numbers(other._numbers), _maxSize(other._maxSize) {}
 
 Span::~Span() {
 	std::cout << "Destructor for Span has been called" << std::endl;
@@ -16,8 +14,31 @@ Span::~Span() {
 
 Span &Span::operator=(const Span &rhs) {
 	if (this != &rhs) {
+		_numbers = rhs._numbers;
+		_maxSize = rhs._maxSize;
 	}
 	return *this;
 	std::cout << "Copy assignement operator called." << std::endl;
 }
+
+void Span::addNumber(unsigned int num) {
+	if (_numbers.size() >= _maxSize )
+		throw std::runtime_error("Span is full");
+	_numbers.push_back(num);
+}
+
+int Span::shortestSpan() const {
+	
+}
+
+
+int Span::longestSpan() const {
+	if (_numbers.size() < 2)
+		throw std::runtime_error("No enough numbers to found a Span.");
+	int min = *std::min_element(_numbers.begin(), _numbers.end());
+	int max = *std::max_element(_numbers.begin(), _numbers.end());
+
+	return max - min;
+}
+
 
--- 08/ex01/Span.hpp
+++ 08/ex01/Span.hpp
@@ -2,19 +2,31 @@
 # define SPAN_HPP
 # include <iostream>
 # include <string>
-# include <fstream>
+# include <fstream> 
+# include <vector>
+# include <algorithm>
 
 class Span {
 private:
-
-
+	std::vector<int> _numbers;
+	unsigned int _maxSize;
 public:
 	Span();
-	Span();
+	Span(int n);
 	Span(const Span &other);
 	~Span();
 
 	Span &operator=(const Span &rhs);
+
+	void addNumber(unsigned int num);
+	template <typename It>
+	void addNumber(It begin, It end) {
+		if (_numbers.size() + std::distance(begin, end) > _maxSize)
+			throw std::runtime_error("Span has not enough space");
+		_numbers.insert(_numbers.end(), begin, end);
+	}
+	int shortestSpan() const;
+	int longestSpan() const;
 
 };
 
