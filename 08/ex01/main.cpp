048d5b418396d74ef2cd32f20cbdfc775d42f7f2
aavduli <aavduli@42lausanne.ch>
2025-04-18 10:23:39 +0200

    adding subject test

--- 08/ex01/main.cpp
+++ 08/ex01/main.cpp
@@ -18,12 +18,31 @@ int main() {
 	std::cout << "Longest Span: " << a.longestSpan() << std::endl;
 	}
 	{
-		Span b(10000);
-		std::vector<int> toAdd;
-		for (int i = 0; i < 10000; ++i)
-			toAdd.push_back(i);
-		b.addNumber(toAdd.begin(), toAdd.end());
-		std::cout << "Shortest Span: " << b.shortestSpan() << std::endl;
-		std::cout << "Longest Span: " << b.longestSpan() << std::endl;
+		try {
+			Span b(10000);
+			std::vector<int> toAdd;
+			for (int i = 0; i < 10000; ++i)
+				toAdd.push_back(i);
+			b.addNumber(toAdd.begin(), toAdd.end());
+			std::cout << "Shortest Span: " << b.shortestSpan() << std::endl;
+			std::cout << "Longest Span: " << b.longestSpan() << std::endl;
+		} catch (std::exception& e) {
+			std::cout << "Exception: " << e.what() << std::endl;
+		}
+	}
+	{
+		try {
+			std::cout << "=====subject test=====\n";
+			Span sp = Span(5);
+			sp.addNumber(6);
+			sp.addNumber(3);
+			sp.addNumber(17);
+			sp.addNumber(9);
+			sp.addNumber(11);
+			std::cout << sp.shortestSpan() << std::endl;
+			std::cout << sp.longestSpan() << std::endl;
+		} catch (std::exception& e) {
+			std::cout << "Exception: " << e.what() << std::endl;
+		}
 	}
 }
