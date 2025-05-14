1e6994fcb1767d437aa038fb97eb4dc6d58cacef
Avdu <avduli.avdul@gmail.com>
2025-05-14 13:36:53 +0200

    Add test for Iter

--- 07/ex01/main.cpp
+++ 07/ex01/main.cpp
@@ -4,9 +4,15 @@ void printInt(int& x) {
 	std::cout << x << std::endl;
 }
 
+void setInt(int& x) {
+	x = 4;
+}
+
 int main() {
 	srand(time(NULL));
 	int arr[5] = {rand()%10, rand()%10, rand()%10 , rand()%10, rand()%10};
+	iter(arr, 5, printInt);
+	iter(arr, 5, setInt);
 	iter(arr, 5, printInt);
 
 	return 0;
--- 07/ex01/template.hpp
+++ 07/ex01/template.hpp
@@ -6,6 +6,8 @@
 
 template <typename T>
 void iter(T* array, int lenght, void (*f)(T&)) {
+	if (!array || lenght <= 0)
+		return ;
 	for (int i = 0; i < lenght; i++)
 		f(array[i]);
 }
