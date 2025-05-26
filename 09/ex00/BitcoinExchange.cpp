64f7def6428789051e74afc5dad42da3a17e38ab
aavduli <aavduli@42lausanne.ch>
2025-05-26 14:39:43 +0200

    go

--- 09/ex00/BitcoinExchange.cpp
+++ 09/ex00/BitcoinExchange.cpp
@@ -6,8 +6,10 @@ BitcoinExchange::~BitcoinExchange() {}
 
 void BitcoinExchange::loadDatabase(const std::string& filename) {
 	std::ifstream file(filename.c_str());
-	if (!file.is_open())
+	if (!file.is_open()) {
 		throw std::runtime_error("Could not open the DB file");
+		return ;
+	}
 	std::string line;
 	std::getline(file, line);
 	while (std::getline(file, line)) {
@@ -24,6 +26,7 @@ void BitcoinExchange::processInput(const std::string& filename) {
 	std::ifstream file(filename.c_str());
 	if (!file.is_open()) {
 		throw std::runtime_error("Could not open input file");
+		return ;
 	}
 	std::string line;
 	std::getline(file, line);
--- 09/ex00/main.cpp
+++ 09/ex00/main.cpp
@@ -1,5 +1,17 @@
 #include "BitcoinExchange.hpp"
 
-int main() {
+int main(int ac, char **av) {
+	if (ac != 2) {
+		std::cout << "Missing arguments: <file>\n";
+		return 1;
+	}
+	try {
+		BitcoinExchange bc;
+		bc.loadDatabase("data.csv");
+		bc.processInput(av[1]);
+	} catch (std::exception &e) {
+		std::cout << "Exception: " << e.what() << std::endl;
+	}
+	return 0;
 }
 
