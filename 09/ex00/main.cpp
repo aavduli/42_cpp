ad9e77ce31941c9274f27412f0a0349bd8bc6cf5
aavduli <aavduli@42lausanne.ch>
2025-07-16 12:59:02 +0200

    no changes

--- 09/ex00/main.cpp
+++ 09/ex00/main.cpp
@@ -2,7 +2,7 @@
 
 int main(int ac, char **av) {
 	if (ac != 2) {
-		std::cout << "Please insert an input file" << std::endl;
+		std::cout << "Please insert a valid input file" << std::endl;
 		return 1;
 	}
 	BitcoinExchange bc;
